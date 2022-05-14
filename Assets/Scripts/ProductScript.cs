034b552d4a5cfc11d790899bbd7f52661373cfdd
yorik338 <96735382+yorik338@users.noreply.github.com>
2022-05-14 19:50:20 +0300

    Накидал примерные рамки магазина

---
+++ Assets/Scripts/ProductScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductScript : ScriptableObject
+{
+    public  string nameofProduct;
+    public int Cost;
+    public int Attack;
+    public int Health;
+    public int Defend;
+    public Sprite artwork;
+
+}
