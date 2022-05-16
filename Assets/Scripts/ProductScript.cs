4013b24d9b48bf91baba767d8bb07535f7855e9e
yorik338 <96735382+yorik338@users.noreply.github.com>
2022-05-16 13:45:52 +0300

    2 версия магазина

--- Assets/Scripts/ProductScript.cs
+++ Assets/Scripts/ProductScript.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName ="New Product",menuName ="ProductforMarket" )]
 public class ProductScript : ScriptableObject
 {
     public  string nameofProduct;
-    public int Cost;
-    public int Attack;
-    public int Health;
-    public int Defend;
+    public int cost;
+    public int attack;
+    public int health;
+    public int attackspeed;
+    public int movespeed;
     public Sprite artwork;
+    public bool isbought;
+    public bool ischoosen;
 
 }
