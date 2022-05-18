ca8715cee07014b78fc250a901636db634dc7b06
yorik338 <96735382+yorik338@users.noreply.github.com>
2022-05-18 18:58:50 +0300

    2.2  Версия м

--- Assets/Scripts/MarketScript.cs
+++ Assets/Scripts/MarketScript.cs
@@ -10,18 +10,24 @@ public class MarketScript : MonoBehaviour
     [SerializeField] private Image mainimage;
     [SerializeField] ProductScript[] Products;
     [SerializeField] private Text buttontext;
+    [SerializeField] private GameObject slot;
+    [SerializeField] private GameObject parentslot;
     private int i = 0;
     private void Awake()
     {
         UpdateInfo();
-        
+        for(int d = 0; d == Products.Length; )
+        {
+            GameObject newProducts = Instantiate(slot,  parentslot.transform );
+            
+        }
     }
     public void UpdateInfo()
     {
         nametext.text = Products[i].name;
         chtext.text = Products[i].health.ToString() + "\n" + Products[i].attack.ToString() + "\n" + Products[i].attackspeed.ToString() + "\n" + Products[i].movespeed.ToString() +"\n" + Products[i].cost.ToString();
         mainimage.sprite = Products[i].artwork;
-        buttontext.text = Products[i].isbought == true ? Products[0].ischoosen == true ? "Choosen": "Choose" : "Buy";
+        buttontext.text = Products[i].isbought == true ? Products[i].ischoosen == true ? "Choosen": "Choose" : "Buy";
 
         
     }
